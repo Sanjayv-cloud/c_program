2866e53df6de0a8a3004f0d9760d2b024f7d84b3
gaming057 <vijaysanjay057@gmail.com>
2023-09-11 00:04:27 +0530

    add new sna assignment

---
+++ c programs/sna/fulprymid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+
+    int row;
+    printf("enter no of row you want :");
+    scanf("%d",&row);
+    for(int i =1;i<=row;i++){
+
+        for(int j = 1;j<=2*row-1;j++){
+
+            if(j>=row-(i-1)&&j<=row+(i-1)){
+
+                printf("*");
+            }
+            else{
+                printf(" ");}
+            }
+            printf("\n");
+        }
+    }
